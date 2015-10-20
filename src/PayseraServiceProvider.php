0f8b47e9f6bab9a659ea937c72e4b6a48d009732
Algirdas <algirdasdum@gmail.com>
2015-10-20 16:07:47 +0300

    First commit

---
+++ src/PayseraServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Artme\Paysera;
+
+use Illuminate\Support\ServiceProvider;
+
+include __DIR__.'/routes.php';
+
+class PayseraServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Artme\Paysera\PayseraController');
+        require_once(__DIR__.'/../lib/WebToPay.php');
+    }
+}
